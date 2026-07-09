de67ce8f113a6690ae55f86a733ce500c3f9d3a8
gptshubham595 <gptshubham595@gmail.com>
2026-07-09 17:17:16 +0530

    updated

--- fcm-backend/send.js
+++ fcm-backend/send.js
@@ -5,7 +5,7 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-const REGISTRATION_TOKEN = "dem4jpr_TAqx2h0tjbtoGc:APA91bGwXKG2Y_cLJxS7yEL7utpe2L7jf012i8lZY2kSNC4aDvgzc1-3MgIsUHrB-qOjO48qT5LW5XbopyqK19M8tYb9aqfSqBUmDgXWNwR0SvBfrFOg_to";
+const REGISTRATION_TOKEN = "TOKEN";
 
 // Construct a data-only payload to force client-side processing in all app states
 const message = {
